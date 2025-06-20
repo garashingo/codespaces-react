cb4bdfdc9857ccd41dd8d3671f11feb727032674
garashingo <garashingo@gmail.com>
2025-06-20 18:29:26 +0000

    ミニゲームを追加

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,9 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
+import ShootingGame from "./ShootingGame";  // ←追記
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
   const [name, setName] = useState("");
 
   useEffect(() => {
@@ -23,30 +22,17 @@ function App() {
         liffId: import.meta.env.VITE_LIFF_ID
       })
       .then(() => {
-        setMessage("LIFF init succeeded.");
         liff.getProfile()
           .then((profile) => {
             setName(profile.displayName);
           })
-          .catch((err) => {
-            console.log("error", err);
-          });
       })
-      .catch((e) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
-  });
-
+  }, []);
+  
   return (
     <div className="App">
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
       {name && <p>こんにちは、{name}さん</p>}
+      <ShootingGame />  {/* 追記 */}
     </div>
   );
 }
